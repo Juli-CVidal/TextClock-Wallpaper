fef8a13660da926da8d4b8a150caf8f311557425
Juli-CVidal <juli.vidal.019@gmail.com>
2023-01-21 17:19:32 -0300

    Uncommented interval

--- js/index.js
+++ js/index.js
@@ -95,8 +95,8 @@ function updateHour(classes) {
   updateElements(".hr", classes, "active");
 }
 
-// setInterval(function () {
-//   textClock();
-// }, 1000);
+setInterval(function () {
+  textClock();
+}, 1000);
 
 textClock();
