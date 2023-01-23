ac60dfb7117d0325c710563e56aed331dcfb4385
Juli-CVidal <juli.vidal.019@gmail.com>
2023-01-23 12:31:24 -0300

    Modified ranges between minute descriptions

--- js/index.js
+++ js/index.js
@@ -20,19 +20,34 @@ const HOURS_OBJ = {
 };
 
 const MINUTE_DESCRIPTIONS = [
-  { from: "0:00", to: "5:00", description: "oclock" },
-  { from: "5:00", to: "10:00", description: "five past" },
-  { from: "10:00", to: "15:00", description: "ten past" },
-  { from: "15:00", to: "20:00", description: "quarter past" },
-  { from: "20:00", to: "25:00", description: "twenty past" },
-  { from: "25:00", to: "30:00", description: "twenty five past" },
-  { from: "30:00", to: "35:00", description: "half past" },
-  { from: "35:00", to: "40:00", description: "twenty five to" },
-  { from: "40:00", to: "45:00", description: "twenty to" },
-  { from: "45:00", to: "50:00", description: "quarter to" },
-  { from: "50:00", to: "55:00", description: "ten to" },
-  { from: "55:00", to: "60:00", description: "five to" },
-];
+  { from: "57:30", to: "2:30", description: "oclock" },
+  { from: "2:30", to: "7:30", description: "five past" },
+  { from: "7:30", to: "12:30", description: "ten past" },
+  { from: "12:30", to: "17:30", description: "quarter past" },
+  { from: "17:30", to: "22:30", description: "twenty past" },
+  { from: "22:30", to: "27:30", description: "twenty five past" },
+  { from: "27:30", to: "32:30", description: "half past" },
+  { from: "32:30", to: "37:30", description: "twenty five to" },
+  { from: "37:30", to: "42:30", description: "twenty to" },
+  { from: "42:30", to: "47:30", description: "quarter to" },
+  { from: "47:30", to: "52:30", description: "ten to" },
+  { from: "52:30", to: "57:30", description: "five to" },
+  ];
+
+// const MINUTE_DESCRIPTIONS = [
+//   { from: "0:00", to: "5:00", description: "oclock" },
+//   { from: "5:00", to: "10:00", description: "five past" },
+//   { from: "10:00", to: "15:00", description: "ten past" },
+//   { from: "15:00", to: "20:00", description: "quarter past" },
+//   { from: "20:00", to: "25:00", description: "twenty past" },
+//   { from: "25:00", to: "30:00", description: "twenty five past" },
+//   { from: "30:00", to: "35:00", description: "half past" },
+//   { from: "35:00", to: "40:00", description: "twenty five to" },
+//   { from: "40:00", to: "45:00", description: "twenty to" },
+//   { from: "45:00", to: "50:00", description: "quarter to" },
+//   { from: "50:00", to: "55:00", description: "ten to" },
+//   { from: "55:00", to: "60:00", description: "five to" },
+// ];
 
 function roundToNearest() {
   let result = "";
@@ -68,10 +83,6 @@ function setDate() {
 
 function textClock() {
   setDate();
-  if (day == 0) {
-    document.querySelector("#name").innerHTML = "BOBCAT";
-    // $('#name').html('BOBCAT');
-  }
   updateHour(HOURS_OBJ[hours]);
   updateDesc(roundToNearest());
 }
